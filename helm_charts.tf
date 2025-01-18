0e49189241b531245163d0eb5464d6bb9a36d641
Colan Schwartz <13228-colan@users.noreply.gitlab.com>
2025-01-18 16:21:16 -0500

    Refs #142: Added PVC for the Drupal files system.

--- helm_charts.tf
+++ helm_charts.tf
@@ -7,4 +7,33 @@ resource "helm_release" "bitnami_drupal" {
 
   # TODO: Set values as per
   # https://github.com/bitnami/charts/tree/main/bitnami/drupal#parameters
+  values = [
+    # Terraform planning diffs don't show up as nicely with YAML, but at least
+    # we can place comments in here.  Temporarily switch to jsonencode() if you
+    # need a cleaner diff.
+    yamlencode({
+      image = {
+        registry   = "registry.gitlab.com"
+        repository = "backupscale/infrastructure/drupal-dashboard-${var.environment}:latest"
+        pullPolicy = "Always"
+      }
+      persistence = {
+        enabled = false
+      }
+      extraVolumes = [
+        {
+          name = var.drupal_files_volume_name
+          persistentVolumeClaim = {
+            claimName = var.drupal_files_pvc_name
+          }
+        }
+      ]
+      extraVolumeMounts = [
+        {
+          name      = var.drupal_files_volume_name
+          mountPath = "${var.drupal_root_directory}/${var.drupal_files_directory}"
+        }
+      ]
+    })
+  ]
 }
--- variables.tf
+++ variables.tf
@@ -2,9 +2,36 @@
 variable "cluster_terraform_id" {}
 variable "environment" {}
 variable "helm_chart_version" {}
+variable "drupal_files_storage_class" {}
 
 # Optionals with defaults.
 variable "drupal_dashboard_namespace" {
   type    = string
   default = "dash"
+}
+ variable "drupal_files_volume_name" {
+   type    = string
+   default = "drupal-files"
+ }
+variable "drupal_root_directory" {
+  type    = string
+  default = "/opt/bitnami/drupal"
+}
+variable "drupal_files_directory" {
+  type    = string
+  default = "sites/default/files"
+}
+variable "drupal_files_storage_size" {
+  type    = string
+  default = "2Gi"
+}
+variable "drupal_files_pvc_name" {
+  type    = string
+  default = "drupal-files-pvc"
+}
+
+# Indicators.
+variable "longhorn_ready" {
+  description = "Indicator that Longhorn is ready and deployed."
+  type        = string
 }
