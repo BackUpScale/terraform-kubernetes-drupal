8eba264f820989328a361e129825cbad81ef8d18
Colan Schwartz <13228-colan@users.noreply.gitlab.com>
2025-01-18 20:34:25 -0500

    Refs #142: Added PVC for Drupal file system (with the defn. this time!)

---
+++ volumes.tf
@@ -0,0 +1,16 @@
+resource "kubernetes_persistent_volume_claim" "drupal_files_pvc" {
+  metadata {
+    name      = var.drupal_files_pvc_name
+    namespace = kubernetes_namespace.drupal_dashboard.metadata[0].name
+  }
+  spec {
+    access_modes = ["ReadWriteMany"]
+    resources {
+      requests = {
+        storage = var.drupal_files_storage_size
+      }
+    }
+    storage_class_name = var.drupal_files_storage_class
+  }
+  depends_on = [var.longhorn_ready]
+}
