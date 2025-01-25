b9be8b0cb5f243a385dd3d067648482e537b0a91
Colan Schwartz <13228-colan@users.noreply.gitlab.com>
2025-01-25 11:42:26 -0500

    Refs #142: Fixed a bad version dependency & upgraded Terraform modules.

--- main.tf
+++ main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     kubernetes = {
       source  = "hashicorp/kubernetes"
-      version = "~> 1.23"
+      version = "~> 2.31"
     }
     civo = {
       source = "civo/civo"
