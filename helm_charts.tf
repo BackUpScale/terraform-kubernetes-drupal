a6b78f4d4e00fc04e0c3bd81da3ab4355666bbdd
Colan Schwartz <13228-colan@users.noreply.gitlab.com>
2025-03-23 11:11:38 -0400

    Refs #142: Changed the access mode for the Drupal PVC so multiple nodes can write.

--- helm_charts.tf
+++ helm_charts.tf
@@ -33,6 +33,7 @@ resource "helm_release" "bitnami_drupal" {
         # Do we want the default PVC?
         persistence = {
           enabled = true
+          accessModes = ["ReadWriteMany"]
         }
         extraEnvVars = [
           {
