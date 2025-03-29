147cc286aaa074cd5a3888faf1c6c97e36e59d2b
Colan Schwartz <13228-colan@users.noreply.gitlab.com>
2025-03-29 18:33:52 -0400

    Refs #142: Commented out the ingress stuff as we don't need it yet.

--- ingress.tf
+++ ingress.tf
@@ -1,34 +1,34 @@
-resource "kubernetes_ingress" "drupal_ingress" {
-  metadata {
-    name      = "drupal-ingress"
-    namespace = kubernetes_namespace.drupal_dashboard.metadata[0].name
-    annotations = {
-      "kubernetes.io/ingress.class" = "civo"
-      "cert-manager.io/cluster-issuer" = "letsencrypt-${var.environment}"
-      "nginx.ingress.kubernetes.io/force-ssl-redirect" = "true"
-    }
-  }
-  spec {
-    tls {
-      hosts       = [var.drupal_domain]
-      secret_name = "drupal-tls"
-    }
-    rule {
-      host = var.drupal_domain
-      http {
-        path {
-          path      = "/"
-          path_type = "Prefix"
-          backend {
-            service {
-              name = kubernetes_service.drupal_service.metadata[0].name
-              port {
-                number = 80
-              }
-            }
-          }
-        }
-      }
-    }
-  }
-}
+# resource "kubernetes_ingress" "drupal_ingress" {
+#   metadata {
+#     name      = "drupal-ingress"
+#     namespace = kubernetes_namespace.drupal_dashboard.metadata[0].name
+#     annotations = {
+#       "kubernetes.io/ingress.class" = "civo"
+#       "cert-manager.io/cluster-issuer" = "letsencrypt-${var.environment}"
+#       "nginx.ingress.kubernetes.io/force-ssl-redirect" = "true"
+#     }
+#   }
+#   spec {
+#     tls {
+#       hosts       = [var.drupal_domain]
+#       secret_name = "drupal-tls"
+#     }
+#     rule {
+#       host = var.drupal_domain
+#       http {
+#         path {
+#           path      = "/"
+#           path_type = "Prefix"
+#           backend {
+#             service {
+#               name = kubernetes_service.drupal_service.metadata[0].name
+#               port {
+#                 number = 80
+#               }
+#             }
+#           }
+#         }
+#       }
+#     }
+#   }
+# }
